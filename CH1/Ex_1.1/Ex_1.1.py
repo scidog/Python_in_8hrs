ce8676222c1bb4eb2ca82b4234311aa2658d42ff
gunmonkey457 <scidog@gmail.com>
2018-09-17 23:01:14 -0400

    first successful change to my 1st branch

--- CH1/Ex_1.1/Ex_1.1.py
+++ CH1/Ex_1.1/Ex_1.1.py
@@ -1,4 +1,5 @@
 # Example 1.1
 print ("Hello World!")
 print ("Dang! Bax actually made a GitHub Repository. Alert the media!")
-print ("No man! He really did it!")
+print ("No man! He really did it man!")
+print (LOOKS LIKE I JUST MADE MY 1ST BRANCH!)
