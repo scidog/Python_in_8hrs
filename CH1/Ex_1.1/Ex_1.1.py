b27d74684c8fb42e46bd84baaa1e5e66b474ffd7
gunmonkey457 <scidog@gmail.com>
2018-09-18 20:24:50 -0400

    added quotes to get rid of print statement error

--- CH1/Ex_1.1/Ex_1.1.py
+++ CH1/Ex_1.1/Ex_1.1.py
@@ -2,4 +2,4 @@
 print ("Hello World!")
 print ("Dang! Bax actually made a GitHub Repository. Alert the media!")
 print ("No man! He really did it man!")
-print (LOOKS LIKE I JUST MADE MY 1ST BRANCH!)
+print ("LOOKS LIKE I JUST MADE MY 1ST BRANCH!")
