09a488c1ab1752ebea3fb830a326e86b1dda1535
gunmonkey457 <scidog@gmail.com>
2018-09-18 21:48:25 -0400

    corrected some errors in Ex_1.4

--- CH1/Ex_1.4/Ex_1.4.py
+++ CH1/Ex_1.4/Ex_1.4.py
@@ -1,5 +1,5 @@
 # Ex_1.4.py
-var01, var02, var03, var03, var05 = 1,,2, 3, 4, 5
+var01, var02, var03, var04, var05 = 1, 2, 3, 4, 5
 print ("var01 = ", var01)
 print ("var02 = ", var02)  
 print ("var03 = ", var03)  
