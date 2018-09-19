cf9cfed744da2644a72ca721f4ba5c8b7b207080
gunmonkey457 <scidog@gmail.com>
2018-09-18 21:38:12 -0400

    added some changes to masterin Ex_1.4

--- CH1/Ex_1.4/Ex_1.4.py
+++ CH1/Ex_1.4/Ex_1.4.py
@@ -1,3 +1,7 @@
 # Ex_1.4.py
-var01 = 100
+var01, var02, var03, var04 = 100, 200, 300, 400
+
 print ("var01 = ", var01)  
+print ("var02 = ", var02) 
+print ("var03 = ", var03)
+print ("var04 = ", var04) 
