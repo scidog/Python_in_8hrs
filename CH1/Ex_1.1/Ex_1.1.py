cb158343323d6ac57d0fa58fc3fe03b15ed76c96
gunmonkey457 <scidog@gmail.com>
2018-09-18 20:34:42 -0400

    added ...man! to master

--- CH1/Ex_1.1/Ex_1.1.py
+++ CH1/Ex_1.1/Ex_1.1.py
@@ -1,4 +1,4 @@
 # Example 1.1
 print ("Hello World!")
 print ("Dang! Bax actually made a GitHub Repository. Alert the media!")
-print ("No man! He really did it!")
+print ("No man! He really did it...man!")
