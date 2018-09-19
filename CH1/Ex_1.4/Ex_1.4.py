778a617c5155df3ae577d5a8bb311a0dac4cdd55
gunmonkey457 <scidog@gmail.com>
2018-09-18 21:47:10 -0400

    changed some vars in Ex_1.4

--- CH1/Ex_1.4/Ex_1.4.py
+++ CH1/Ex_1.4/Ex_1.4.py
@@ -1,3 +1,7 @@
 # Ex_1.4.py
-var01 = 100
-print ("var01 = ", var01)  
+var01, var02, var03, var03, var05 = 1,,2, 3, 4, 5
+print ("var01 = ", var01)
+print ("var02 = ", var02)  
+print ("var03 = ", var03)  
+print ("var04 = ", var04)  
+print ("var05 = ", var05)  
