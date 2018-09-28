e63ed08cb14bdde53e66ad067a6169bc8e6f2488
gunmonkey457 <scidog@gmail.com>
2018-09-27 22:29:07 -0400

    Ex_1. 12.py-branch-01 Teaked 1.12

--- CH1/Ex_1.12/Ex_1.12.py
+++ CH1/Ex_1.12/Ex_1.12.py
@@ -1,7 +1,10 @@
 # Example 1.12
 num1 = int(8.67) 
 print (num1)     # returns 8
-num2 = round(8.67)
+num2 = round(8.67654, 3)
 print (num2)    # returns 9.0
 num3 = float(5)
 print (num3)    # returns 5.0
+baxpi = 3.141592653589793
+num4 = round(baxpi, 7)
+print (num4) 
