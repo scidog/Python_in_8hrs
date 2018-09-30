6844e1145147a59ab6b1819bec4d05372d14f46d
gunmonkey457 <scidog@gmail.com>
2018-09-30 16:35:03 -0400

    Added Ex2.3.py

---
+++ CH2/Ex_2.3/Ex2.3.py
@@ -0,0 +1,10 @@
+#Ex2.3.py
+num = 2
+print()
+print ("The answer is:")
+if num < 1:
+    print ("num < 1")
+elif 1 < num < 1.5:
+    print ("num =", num, "1 < num < 1.5")
+else:
+    print ("1.5 < num")
