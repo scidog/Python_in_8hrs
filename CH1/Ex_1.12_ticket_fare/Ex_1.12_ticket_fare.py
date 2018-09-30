ac7b6b0f7bb89452c79bd75676fb6e06f8c5840d
gunmonkey457 <scidog@gmail.com>
2018-09-30 16:48:52 -0400

    Added Ex_1.12_ticket_fare.py

---
+++ CH1/Ex_1.12_ticket_fare/Ex_1.12_ticket_fare.py
@@ -0,0 +1,11 @@
+# Ex_1.12_ticket_fare
+age = 15
+ticket = "Child Fare" if (age < 16) else "Adult Fare"
+print()
+print ("Ticket Fare is:", ticket)
+print()
+num = 15
+if num < 16:
+    print ("num =", num, "and num meet criteria of 15 or less")
+else:
+    print ("num =", num, "and num fails criteria of 15 or less")
