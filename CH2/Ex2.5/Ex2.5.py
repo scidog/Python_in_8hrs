d32c79ada0e38daefe63910a9df517c52a2ba6b7
gunmonkey457 <scidog@gmail.com>
2018-09-30 23:49:42 -0400

    Added and tweaked Ex2.5

---
+++ CH2/Ex2.5/Ex2.5.py
@@ -0,0 +1,20 @@
+#Ex2.5.py
+print()
+print("Begin")
+for var1 in range(7):
+    print ("var1 =:", var1)
+print()
+for var2 in range (3,10):
+    print ("var2 =:", var2)
+print()
+for var3 in range (3,10,2):
+    print ("var3 =:", var3)
+print()
+for var4 in range (10,1,-1):
+    print ("var4 =:", var4)
+print()
+for var5 in range (10,3,-2):
+    print ("var5 =:", var5)
+print()
+print("Done!")
+
