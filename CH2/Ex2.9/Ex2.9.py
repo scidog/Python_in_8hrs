2debe22320d2d421ec3f89ffada591eda2497c0b
gunmonkey457 <scidog@gmail.com>
2018-10-07 21:21:31 -0400

    Added Ex2.9.py

---
+++ CH2/Ex2.9/Ex2.9.py
@@ -0,0 +1,17 @@
+#Ex2.9.py
+'''
+Text input and Raw Input
+'''
+name = input("Name please: ")
+print("Name entered was: " + name)
+age = input ("Age please: ")
+print("Age Entered was: "+ age)
+
+'''
+raw_input is archaic and seems to not even work in some GUIs
+
+rawname = raw_input("Name please [raw btw]: ")
+print("Raw name is: "+ rawname)
+rawage = raw_input("Age please [raw btw]: ")
+print("Raw age is: "+ rawage)
+'''
