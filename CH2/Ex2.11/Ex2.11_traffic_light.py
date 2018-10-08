f17f134d98eb7c3b50e96cbec0ca3101bad11fb8
gunmonkey457 <scidog@gmail.com>
2018-10-07 22:27:08 -0400

    Made prog Ex2.11_traffic_light.py

---
+++ CH2/Ex2.11/Ex2.11_traffic_light.py
@@ -0,0 +1,16 @@
+#Ex2.11_traffic_light.py
+'''
+Traffic Light program using If-elif-else statement
+'''
+print()
+light = input("Is light red, green, or yellow? ")
+if light == "red":
+    print("light is " + light)
+elif light == "green":
+    print("light is " + light)
+elif light == "yellow":
+    print("light is " + light)   
+else:
+    print("light is illegal and of color " + light)
+print()
+    
