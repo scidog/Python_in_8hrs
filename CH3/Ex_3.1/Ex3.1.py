ed0b5061108e220af9082cc6a2f387793f7b0bd4
gunmonkey457 <scidog@gmail.com>
2018-10-07 22:56:20 -0400

    Added Ex3.1.py with text input from me

---
+++ CH3/Ex_3.1/Ex3.1.py
@@ -0,0 +1,17 @@
+#Ex3.1.py
+#Here import function module 'math' and use some of it's functions.
+import math
+print()
+print("degrees(100): ", math.degrees(100))
+print("degrees(10): ", math.degrees(10))
+print("degrees(math.pi): ", math.degrees(math.pi))
+print()
+'''
+here I try some input stuff
+'''
+print()
+print("Value of Pi in radians is: ", + math.pi)
+baxradians = input("Input value in radians: ")
+print("baxradians = ", float(baxradians))
+print("degrees(baxradians) = ", math.degrees(float(baxradians)))
+print()
