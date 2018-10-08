ee794377db642a1383815cc92f58534f215d1c4b
gunmonkey457 <scidog@gmail.com>
2018-10-07 22:13:11 -0400

    Added pass statement ex as Ex2.10.py

---
+++ CH2/Ex2.10/Ex2.10.py
@@ -0,0 +1,13 @@
+#Ex2.10.py
+'''
+Pass statement
+'''
+condition = True
+if condition:
+    print("Condition True")
+elif True:
+    print("pass for now")
+    pass
+else:
+    print("pass for now")
+    pass
